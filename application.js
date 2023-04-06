3b7c3b8274dfa25739aece2bdbbcf69296be86c0
dpsiu <denversiu24@gmail.com>
2023-04-06 15:42:00 -0500

    feat: add 5 round game function

--- application.js
+++ application.js
@@ -1,4 +1,3 @@
-console.log(2+2);
 
 let playerScore = 1;
 let computerScore = 1;
@@ -19,21 +18,42 @@ let computerSelection = getComputerChoice(gameOptions);
 let playerSelection = '';
 
 /*Adds functionality to buttons. Assigns the player's selection. */
-if (rockButton.addEventListener('click', () => playRound('Rock', getComputerChoice(gameOptions)))){
-    console.log(playRound(playerSelection, computerSelection));
+rockButton.addEventListener('click', () => playRound('Rock', getComputerChoice(gameOptions)));
+paperButton.addEventListener('click', () => playRound('Paper', getComputerChoice(gameOptions)));
+scissorsButton.addEventListener('click', () => playRound('Scissors', getComputerChoice(gameOptions)));
+
+/*Restart button to appear at the end of a game with functionality to reload page/game. */
+restartButton.addEventListener('click', () => window.location.reload());
+document.getElementById("restartButton").style.visibility = "hidden";
+
+
+
+/*
+getPlayerSelection();
+
+function getPlayerSelection(){
+    playerSelection = prompt("Do you choose Rock, Paper, or Scissors?");
+    if (playerSelection.toLowerCase === 'rock'.toLowerCase){
+        playRound('Rock', getComputerChoice(gameOptions));
+        console.log("rock");
+    }
+    else if (playerSelection.toLowerCase === 'Paper'.toLowerCase){
+        playRound('Paper', getComputerChoice(gameOptions));
+        console.log("paper");
+
+    }    
+    else if (playerSelection.toLowerCase === 'Scissors'.toLowerCase){
+        playRound('Scissors', getComputerChoice(gameOptions));
+        console.log("scissors");
+
+    }
 }
-else if (paperButton.addEventListener('click', () => playRound('Paper', getComputerChoice(gameOptions)))){
-    console.log(playRound(playerSelection, computerSelection));
-}
-else if (scissorsButton.addEventListener('click', () => playRound('Scissors', getComputerChoice(gameOptions))))
-    console.log(playRound(playerSelection, computerSelection));
 
 
-console.log(computerSelection);
-console.log(playerSelection);
 
-/*Plays a round of RPS. Accounts for all 7 outcomes. */
+/*Plays a round of RPS. Accounts for all 7 outcomes. First prompts user for choice. */
 function playRound(playerSelection, computerSelection){
+    /*Compares player and comp selection to retreive round winner */
     if (playerSelection === computerSelection){
         document.getElementById("round").innerHTML = "Tie round.";
     }
@@ -42,53 +62,38 @@ function playRound(playerSelection, computerSelection){
             || playerSelection === 'Scissors' && computerSelection === 'Paper'){
         document.getElementById("playerScore").innerHTML = playerScore ++;
         document.getElementById("round").innerHTML = "You won this round. "  + playerSelection + " beats " + computerSelection + ".";
+        game(playerScore, computerScore);
     }
     else if(computerSelection === 'Rock' && playerSelection === 'Scissors'
             || computerSelection === 'Paper' && playerSelection === 'Rock'
             || computerSelection === 'Scissors' && playerSelection === 'Paper'){
         document.getElementById("computerScore").innerHTML = computerScore ++;
         document.getElementById("round").innerHTML = "You lost this round. "  + computerSelection + " beats " + playerSelection + ".";
-
+        game(playerScore, computerScore);
     }
 }
-
-/* "Game" function */
 
 function game(playerScore, computerScore){
-    while (playerScore < 6 && computerScore < 6){
-        playRound(playerSelection, computerSelection);
+    if (playerScore === 6){
+        document.getElementById("round").innerHTML = "Player wins " + (playerScore-1) + " to " + (computerScore-1) + "!!!";
+        document.getElementById("restartButton").style.visibility = "visible";
+        disableButton();
+    }
+    else if (computerScore === 6){
+        document.getElementById("round").innerHTML = "Computer wins " + (computerScore-1) + " to " + (playerScore-1) + "...";
+        document.getElementById("restartButton").style.visibility = "visible";
+        disableButton();
+
     }
     
-    if (playerScore === 6){
-        document.getElementById("round").innerHTML = "You win " + (playerScore-1) + " - " + (computerScore-1) + "!";
-    }
-    else{
-        document.getElementById("round").innerHTML = "You lose " + (computerScore-1) + " - " + (playerScore-1) + ".";
-    }
 }
 
-game(playerScore, computerScore);
-
-
-
-/* 
-#Here I have the following. 
-function getComputerChoice(gameOptions){
-    return gameOptions[Math.floor(Math.random()*gameOptions.length)];
+function disableButton(){
+    document.querySelector("#rockButton").disabled = true;
+    document.querySelector("#paperButton").disabled = true;
+    document.querySelector("#scissorsButton").disabled = true;
 }
-let gameOptions = ['Rock', 'Paper', 'Scissors'];
+//
 
-#I then assign it to this variable.
-let computerSelection = getComputerChoice(gameOptions);
+//game(playerScore, computerScore);
 
-#When computerSelection is called, it returns the same number repeatedly.
-Rather than a new random number like getComputerChoice(gameOptions);
-Which is what is is equal to. Why?
-
-*/
-
-/*function playRound(playerSelection, computerSelection){
-    const computerSelection = getComputerChoice;
-}
-
-*/
