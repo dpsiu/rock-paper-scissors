d2439265e975be98417b8272a5740dbc806cbef6
dpsiu <denversiu24@gmail.com>
2023-04-11 17:42:23 -0500

    update: complete rps project

--- application.js
+++ application.js
@@ -1,7 +1,6 @@
 
 let playerScore = 1;
 let computerScore = 1;
-let roundNumber = 0;
 
 
 /*Return a random value for computer choice */
@@ -11,81 +10,61 @@ function getComputerChoice(gameOptions){
 let gameOptions = ['Rock', 'Paper', 'Scissors'];
 
 
-
 let computerSelection = getComputerChoice(gameOptions);
 
 /*Player selection is null until a button is pressed */
 let playerSelection = '';
+
 
 /*Adds functionality to buttons. Assigns the player's selection. */
 rockButton.addEventListener('click', () => playRound('Rock', getComputerChoice(gameOptions)));
 paperButton.addEventListener('click', () => playRound('Paper', getComputerChoice(gameOptions)));
 scissorsButton.addEventListener('click', () => playRound('Scissors', getComputerChoice(gameOptions)));
 
+
 /*Restart button to appear at the end of a game with functionality to reload page/game. */
 restartButton.addEventListener('click', () => window.location.reload());
 document.getElementById("restartButton").style.visibility = "hidden";
-
-
-
-/*
-getPlayerSelection();
-
-function getPlayerSelection(){
-    playerSelection = prompt("Do you choose Rock, Paper, or Scissors?");
-    if (playerSelection.toLowerCase === 'rock'.toLowerCase){
-        playRound('Rock', getComputerChoice(gameOptions));
-        console.log("rock");
-    }
-    else if (playerSelection.toLowerCase === 'Paper'.toLowerCase){
-        playRound('Paper', getComputerChoice(gameOptions));
-        console.log("paper");
-
-    }    
-    else if (playerSelection.toLowerCase === 'Scissors'.toLowerCase){
-        playRound('Scissors', getComputerChoice(gameOptions));
-        console.log("scissors");
-
-    }
-}
-
 
 
 /*Plays a round of RPS. Accounts for all 7 outcomes. First prompts user for choice. */
 function playRound(playerSelection, computerSelection){
     /*Compares player and comp selection to retreive round winner */
     if (playerSelection === computerSelection){
-        document.getElementById("round").innerHTML = "Tie round.";
+        document.getElementById("round").textContent = "Tie round.";
     }
     else if(playerSelection === 'Rock' && computerSelection === 'Scissors'
             || playerSelection === 'Paper' && computerSelection === 'Rock'
             || playerSelection === 'Scissors' && computerSelection === 'Paper'){
-        document.getElementById("playerScore").innerHTML = playerScore ++;
-        document.getElementById("round").innerHTML = "You won this round. "  + playerSelection + " beats " + computerSelection + ".";
+        document.getElementById("playerScore").textContent = playerScore ++;
+        document.getElementById("round").textContent = "You won this round. "  + playerSelection + " beats " + computerSelection + ".";
         game(playerScore, computerScore);
+        console.log((playerScore-1), (computerScore-1));
     }
     else if(computerSelection === 'Rock' && playerSelection === 'Scissors'
             || computerSelection === 'Paper' && playerSelection === 'Rock'
             || computerSelection === 'Scissors' && playerSelection === 'Paper'){
-        document.getElementById("computerScore").innerHTML = computerScore ++;
-        document.getElementById("round").innerHTML = "You lost this round. "  + computerSelection + " beats " + playerSelection + ".";
+        document.getElementById("computerScore").textContent = computerScore ++;
+        document.getElementById("round").textContent = "You lost this round. "  + computerSelection + " beats " + playerSelection + ".";
         game(playerScore, computerScore);
+        console.log((playerScore-1), (computerScore-1));
+
     }
 }
 
+
+// Declares a winner once their index = 6 (When their points = 5)
 function game(playerScore, computerScore){
     if (playerScore === 6){
-        document.getElementById("round").innerHTML = "Player wins " + (playerScore-1) + " to " + (computerScore-1) + "!!!";
+        document.getElementById("round").textContent = "Player wins " + (playerScore-1) + " to " + (computerScore-1) + "!!!";
         document.getElementById("restartButton").style.visibility = "visible";
         disableButton();
     }
     else if (computerScore === 6){
-        document.getElementById("round").innerHTML = "Computer wins " + (computerScore-1) + " to " + (playerScore-1) + "...";
+        document.getElementById("round").textContent = "Computer wins " + (computerScore-1) + " to " + (playerScore-1) + "...";
         document.getElementById("restartButton").style.visibility = "visible";
         disableButton();
-
     }
-    
 }
 
 function disableButton(){
@@ -93,7 +72,3 @@ function disableButton(){
     document.querySelector("#paperButton").disabled = true;
     document.querySelector("#scissorsButton").disabled = true;
 }
-//
-
-//game(playerScore, computerScore);
-
